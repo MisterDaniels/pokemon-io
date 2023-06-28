f7ecfa53e63a91f03e8d73d963f30fbcf455edb0
Daniel Silva <dodosilva51@gmail.com>
2023-06-27 23:45:21 -0300

    Added player movement tile-based
    Added tileMap for the first main scene
    
    Created generic function to check for non existing tiles (map offset)

--- src/actors/player.ts
+++ src/actors/player.ts
@@ -1,12 +1,39 @@
-import { Actor, Color, Engine, vec } from "excalibur";
+import { Actor, Color, Engine, Input, vec } from "excalibur";
+import { Main } from "@src/scenes";
 
 export class Player extends Actor {
     constructor() {
         super({
-            pos: vec(150, 150),
+            pos: vec(0, 0),
             width: 32,
             height: 32,
             color: Color.Red
         });
+    }
+
+    update(engine: Engine, delta: number): void {
+        let pos = this.pos.clone();
+
+        if (engine.input.keyboard.wasPressed(Input.Keys.ArrowUp)) {
+            pos.y -= 32;
+        }
+
+        if (engine.input.keyboard.wasPressed(Input.Keys.ArrowDown)) {
+            pos.y += 32;
+        }
+
+        if (engine.input.keyboard.wasPressed(Input.Keys.ArrowLeft)) {
+            pos.x -= 32;
+        }
+
+        if (engine.input.keyboard.wasPressed(Input.Keys.ArrowRight)) {
+            pos.x += 32;
+        }
+
+        if (!pos.equals(this.pos) && (this.scene as Main).isTileBlocked(pos)) {
+            return;
+        }
+
+        this.pos = pos;
     }
 }
--- src/scenes/main/index.ts
+++ src/scenes/main/index.ts
@@ -1,7 +1,29 @@
-import { Engine, Scene, SceneActivationContext } from 'excalibur';
+import { Engine, Scene, SceneActivationContext, TileMap, Vector, vec } from 'excalibur';
 
 export default class Main extends Scene {
+    public tileMap: TileMap;
+
+    constructor() {
+        super();
+
+        this.tileMap = new TileMap({
+            pos: vec(0, 0),
+            tileWidth: 32,
+            tileHeight: 32,
+            rows: 10,
+            columns: 10
+        });
+    }
+
     public onInitialize(_engine: Engine): void {}
     public onActivate(_context: SceneActivationContext<unknown>): void {}
     public onDeactivate(_context: SceneActivationContext<undefined>): void {}
+
+    public isTileBlocked(vec: Vector): boolean {
+        const tile = this.tileMap.getTileByPoint(vec);
+
+        if (!tile) return true;
+
+        return false;
+    }
 }
